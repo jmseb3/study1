a2935d2eaefccef22bc6ceb61a83c539ec645346
jmseb3 <jmseb3@naver.com>
2021-05-25 00:02:27 +0900

    DO IT-안드로이드개발(4)

--- app/src/main/java/com/wonddak/study1/MainActivity.kt
+++ app/src/main/java/com/wonddak/study1/MainActivity.kt
@@ -2,10 +2,19 @@ package com.wonddak.study1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.wonddak.study1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.btnAdd.setOnClickListener {
+            binding.todoItem.text = binding.textInput.text
+            binding.textInput.setText("")
+        }
     }
 }
